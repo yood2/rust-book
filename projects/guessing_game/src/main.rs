3e247e0ea68688aba6b891792ed87ce469796585
yood2 <yood2@proton.me>
2025-03-29 14:33:09 -0700

    chapter 3.3

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,33 +1,40 @@
-use std::io;
+use std::io; //standard library
 use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    loop {
-        println!("Guess the number:");
+    println!("Guess the number!");
 
-        let secret_number = rand::thread_rng().gen_range(1..=10);
-    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // println!("Secret number: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+
+        // let to create variable
+        // mut to make it mutable
+        // String::new() means use the new() associated function with String type
         let mut guess = String::new();
-    
+
+        //&mut guess means mutable reference
+        // reference allows you to use variable without copying
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-    
+        
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-    
-        println!("You guess: {}", guess);
-        println!("The secret number is: {}", secret_number);
-    
+        
+        println!("You guessed: {guess}");
+
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
+            Ordering::Less => println!("Too small!"),  
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
-                break;
+                break; // exit loop
             }
         }
     }
